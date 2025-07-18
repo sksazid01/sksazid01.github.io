3582396a2461542f9eb900f2d782cf1d2bea012c
Md. AHASANUL HAQUE SAZID <107117503+sksazid01@users.noreply.github.com>
2025-07-18 15:14:11 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+<!-- Create index.html file -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Portfolio</title>
+</head>
+<body>
+    <h1>Welcome to My Site!</h1>
+    <p>This is my GitHub Pages website.</p>
+</body>
+</html>
